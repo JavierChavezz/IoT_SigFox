349b8f4efce0f5e87509c65394bd653753853b5d
JavierChavezz <47484752+JavierChavezz@users.noreply.github.com>
2020-03-24 12:23:17 -0600

    Add files via upload

--- Temp_Sensor.ino
+++ Temp_Sensor.ino
@@ -1,6 +1,8 @@
 
 #include "NXTIoT_dev.h"
 #include "DHT.h" 
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 #define DHTPIN 2 
 #define DHTTYPE DHT11 
@@ -9,13 +11,22 @@ DHT dht(DHTPIN, DHTTYPE);
 
 NXTIoT_dev  mysigfox;
 
+
+
 const int boton=6;
 const int sensorPin = A0;
+const int Battery_Pin = A1;
+const int soil_temp_PIN = 3;
+
+// Instancia a las clases OneWire y DallasTemperature
+OneWire oneWireObjeto(soil_temp_PIN);
+DallasTemperature sensorDS18B20(&oneWireObjeto);
 
 void setup() 
 {
   Serial.begin(9600);
-  dht.begin(); //Se inicia el sensor
+  dht.begin(); //Se inician los sensores
+  sensorDS18B20.begin(); 
   pinMode(boton, INPUT);
 }
 
@@ -35,27 +46,56 @@ void loop()
 
 void leer_temperatura()
 {
-  //int sensorVal=analogRead(sensorPin);
-  //float voltaje=(sensorVal/1024.0)*5;
-  float h = dht.readHumidity(); //se lee la humedad
-  float t= dht.readTemperature(); // se lee la temperatura
-  //Serial.print("Voltaje: ");
-  //Serial.println(voltaje); 
+   float Battery_AR    = 0;
+   float soil_hum_AR   = 0;
+   float soil_temp     = 0; 
+   int soil_hum        = 0;
+   int air_hum         = 0;
+   int air_temp        = 0;
+   int Battery_Lvl     = 0;
 
-  Serial.println("Humedad: "); 
-  Serial.println(h); 
+  Battery_AR =  analogRead(Battery_Pin);
+  Battery_Lvl = map(Battery_AR, 0, 1023, 0, 100);
+  
+  sensorDS18B20.requestTemperatures();
+  soil_temp = sensorDS18B20.getTempCByIndex(0);
+  
+  air_hum  = dht.readHumidity(); //se lee la humedad
+  air_temp = dht.readTemperature(); // se lee la temperatura
  
-  Serial.print("Grados Cº: "); 
-  Serial.println(t); 
+  soil_hum_AR = analogRead(sensorPin);
+  soil_hum = map(soil_hum_AR, 0, 1023, 100, 0);
+  
+
+  Serial.print("Humedad del suelo: "); 
+  Serial.print(soil_hum); 
+  Serial.println(" %");
+
+  Serial.print("Temperatura del suelo: "); 
+  Serial.print(soil_temp); 
+  Serial.println(" Grados Cº ");
+  
+  Serial.print("Humedad del aire: "); 
+  Serial.print(air_hum); 
+  Serial.println(" %");
+ 
+  Serial.print("Temperatura del aire: "); 
+  Serial.print(air_temp); 
+  Serial.println(" Grados Cº"); 
+
+  Serial.print("Nivel de Batería: "); 
+  Serial.print(Battery_Lvl); 
+  Serial.println(" %"); 
+  Serial.println(" "); 
+  
 
 
-  //float temp=((voltaje)*100);
-
-   
-  //float temp=(35);
-  //Serial.println(temp);
   mysigfox.initpayload();
-  mysigfox.addfloat(h);
-  mysigfox.addfloat(t);
+
+  mysigfox.addint(air_hum);
+  mysigfox.addint(air_temp);
+  mysigfox.addint(soil_hum);
+  mysigfox.addfloat(soil_temp);
+  mysigfox.addint(Battery_Lvl);
   mysigfox.sendmessage();
 }
